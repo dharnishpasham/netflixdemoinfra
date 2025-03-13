8e160c38fdca36d03420883b84a7fba5e6c2582e
dharnishpasham <awsdevopsfordharnish@gmail.com>
2025-03-13 16:58:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,10 +4,10 @@ region = "us-east-1"
 
 resource "aws_instance" "one" {
 count = 5
-ami = "ami-0ddc798b3f1a5117e"
+ami = "ami-08fe5144e4659a3b3"
 instance_type = "t2.medium"
 key_name = "jenkins123"
-vpc_security_group_ids = ["sg-05f044979e305302e"]
+vpc_security_group_ids = ["sg-065628eded33deb06"]
 tags = {
 Name = var.instance_names[count.index]
 }
